a7569df84c597612152231f5e923d44e39f18391
Antonio1394 <antoniorivm1394@gmail.com>
2016-05-09 12:38:31 -0600

    [add] configure layout

---
+++ WorkCode/resources/views/layouts/script.blade.php
@@ -0,0 +1,14 @@
+ <!-- jQuery -->
+    
+	{!!Html::script('js/jquery.js')!!}
+
+    <!-- Bootstrap Core JavaScript -->
+    {!!Html::script('js/bootstrap.min.js')!!}
+    
+    <!-- Plugin JavaScript -->
+    {!!Html::script('js/jquery.easing.min.js')!!}
+    {!!Html::script('js/jquery.fittext.js')!!}
+    {!!Html::script('js/wow.min.js')!!}
+        <!-- Custom Theme JavaScript -->
+    {!!Html::script('js/creative.js')!!}
+    
